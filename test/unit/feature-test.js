ee87a6cae3ca6d119e27ec6c44cdbd9b2bfcd8a5
Jmeas <jellyes2@gmail.com>
2015-01-17 00:33:24 -0500

    Update tests with Sinon example.

--- test/unit/feature-test.js
+++ test/unit/feature-test.js
@@ -1,5 +1,14 @@
 describe('A feature test', function() {
-  it('should be okay', function() {
-    expect(true).to.be.ok;
+  beforeEach(function() {
+    this.method = stub().returns('hello');
+    this.method();
+  });
+
+  it('should have been run once', function() {
+    expect(this.method).to.have.been.calledOnce;
+  });
+
+  it('should have always returned hello', function() {
+    expect(this.method).to.have.always.returned('hello');
   });
 });
