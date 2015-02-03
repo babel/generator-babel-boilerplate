a4879c02efe8edd55374e03988819aa5e395a401
Jmeas <jellyes2@gmail.com>
2015-02-03 14:25:05 -0500

    Remove comment about wrapper.
    Resolves #69

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-// Place your library's code here
-//
-// If you add additional files, be sure to
-// load them in order in ./wrapper.js
-//
-
 import Another from './another';
 
 const MyLibrary = {
