ecfdb447843818d23ac474fc6ab32af90e6c9a5d
James Kyle <me@thejameskyle.com>
2015-07-30 00:36:42 -0700

    Use more ES6 in gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,28 +1,31 @@
-const gulp = require('gulp');
-const $ = require('gulp-load-plugins')();
-const del = require('del');
-const glob = require('glob');
-const path = require('path');
-const babelify = require('babelify');
-const isparta = require('isparta');
-const esperanto = require('esperanto');
-const browserify = require('browserify');
-const runSequence = require('run-sequence');
-const source = require('vinyl-source-stream');
+import gulp from 'gulp';
+import loadPlugins from 'gulp-load-plugins';
+import del from 'del';
+import glob from 'glob';
+import path from 'path';
+import babelify from 'babelify';
+import isparta from 'isparta';
+import esperanto from 'esperanto';
+import browserify from 'browserify';
+import runSequence from 'run-sequence';
+import source from 'vinyl-source-stream';
 
-const manifest = require('./package.json');
+import manifest from './package.json';
+
+const $ = loadPlugins();
+
 const config = manifest.babelBoilerplateOptions;
 const mainFile = manifest.main;
 const destinationFolder = path.dirname(mainFile);
 const exportFileName = path.basename(mainFile, path.extname(mainFile));
 
 // Remove the built files
-gulp.task('clean', function(cb) {
+gulp.task('clean', cb => {
   del([destinationFolder], cb);
 });
 
 // Remove our temporary files
-gulp.task('clean-tmp', function(cb) {
+gulp.task('clean-tmp', cb => {
   del(['tmp'], cb);
 });
 
@@ -34,7 +37,7 @@ function jscsNotify(file) {
 }
 
 function createLintTask(taskName, files) {
-  gulp.task(taskName, function() {
+  gulp.task(taskName, () => {
     return gulp.src(files)
       .pipe($.plumber())
       .pipe($.eslint())
@@ -52,12 +55,12 @@ createLintTask('lint-src', ['src/**/*.js']);
 createLintTask('lint-test', ['test/**/*.js']);
 
 // Build two versions of the library
-gulp.task('build', ['lint-src', 'clean'], function(done) {
+gulp.task('build', ['lint-src', 'clean'], done => {
   esperanto.bundle({
     base: 'src',
     entry: config.entryFileName,
-  }).then(function(bundle) {
-    var res = bundle.toUmd({
+  }).then(bundle => {
+    let res = bundle.toUmd({
       // Don't worry about the fact that the source map is inlined at this step.
       // `gulp-sourcemaps`, which comes next, will externalize them.
       sourceMap: 'inline',
@@ -82,15 +85,15 @@ gulp.task('build', ['lint-src', 'clean'], function(done) {
 });
 
 // Bundle our app for our unit tests
-gulp.task('browserify', function() {
-  var testFiles = glob.sync('./test/unit/**/*');
-  var allFiles = ['./test/setup/browserify.js'].concat(testFiles);
-  var bundler = browserify(allFiles);
+gulp.task('browserify', () => {
+  let testFiles = glob.sync('./test/unit/**/*');
+  let allFiles = ['./test/setup/browserify.js'].concat(testFiles);
+  let bundler = browserify(allFiles);
   bundler.transform(babelify.configure({
     sourceMapRelative: __dirname + '/src'
   }));
   return bundler.bundle()
-    .on('error', function(err){
+    .on('error', function(err) {
       console.log(err.message);
       this.emit('end');
     })
@@ -105,39 +108,39 @@ function test() {
     .pipe($.mocha({reporter: 'dot', globals: config.mochaGlobals}));
 }
 
-gulp.task('coverage', ['lint-src', 'lint-test'], function(done) {
+gulp.task('coverage', ['lint-src', 'lint-test'], done => {
   require('babel-core/register');
   gulp.src(['src/**/*.js'])
     .pipe($.istanbul({ instrumenter: isparta.Instrumenter }))
     .pipe($.istanbul.hookRequire())
-    .on('finish', function() {
+    .on('finish', () => {
       return test()
-      .pipe($.istanbul.writeReports())
-      .on('end', done);
+        .pipe($.istanbul.writeReports())
+        .on('end', done);
     });
 });
 
 // Lint and run our tests
-gulp.task('test', ['lint-src', 'lint-test'], function() {
+gulp.task('test', ['lint-src', 'lint-test'], () => {
   require('babel-core/register');
   return test();
 });
 
 // Ensure that linting occurs before browserify runs. This prevents
 // the build from breaking due to poorly formatted code.
-gulp.task('build-in-sequence', function(callback) {
+gulp.task('build-in-sequence', callback => {
   runSequence(['lint-src', 'lint-test'], 'browserify', callback);
 });
 
 const watchFiles = ['src/**/*', 'test/**/*', 'package.json', '**/.eslintrc', '.jscsrc'];
 
 // Run the headless unit tests as you make changes.
-gulp.task('watch', function() {
+gulp.task('watch', () => {
   gulp.watch(watchFiles, ['test']);
 });
 
 // Set up a livereload environment for our spec runner
-gulp.task('test-browser', ['build-in-sequence'], function() {
+gulp.task('test-browser', ['build-in-sequence'], () => {
   $.livereload.listen({port: 35729, host: 'localhost', start: true});
   return gulp.watch(watchFiles, ['build-in-sequence']);
 });
